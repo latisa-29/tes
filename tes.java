c538108c4cf3295f94d66699d281d225d5510495
latisa-29 <latisa_sabela_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:57:03 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,6 @@
+public class tes {
+    public static void main(String[]  args) {
+     System.out.println ("Nama saya sasa dari kelas X RPL 7 berasal dari kota Banyuwangi"); 
+     String a = "1"; 
+}
+}
