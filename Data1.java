81230c284d162201b6ef81bbc592b4474f7ae074
latisa-29 <latisa_sabela_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:39:10 +0700

    Add files via upload

---
+++ Data1.java
@@ -0,0 +1,13 @@
+public class Data1 {
+    public static void main(String[] args) {
+        String nama = "sasa";
+        int usia = 16;
+        String alamat = "banyuwangi";
+        double kelas = 10.7;
+
+        System.out.println("nama saya :" + nama);
+        System.out.println("usia :" + usia + " tahun");
+        System.out.println("alamat :" + alamat);
+        System.out.println("kelas :" + kelas);
+    }
+}
