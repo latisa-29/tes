1671a5f9bd4b4b5b827a37362ba65fc19eb5bac8
latisa-29 <latisa_sabela_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:06:06 +0700

    Add files via upload

---
+++ siswa.java
@@ -0,0 +1,16 @@
+//Object class
+public class siswa {
+ int absen;
+ String nama;
+ double ipk;
+
+ //Constructor
+ public siswa (){
+    absen = 0;
+    nama = "kosng";
+    ipk = 0.0;
+
+ }
+}
+
+    
