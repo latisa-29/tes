27e898e65d8117ef02c5e8ff1cae8677fd6bf105
latisa-29 <latisa_sabela_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:06:26 +0700

    Add files via upload

---
+++ siswaTester.java
@@ -0,0 +1,56 @@
+//Driver Class
+public class siswaTester {
+    public static void main(String[] args) {
+        //object
+        //Class object = new Constructor
+        siswa zea = new siswa();
+        siswa nimas = new siswa();
+        siswa talitha = new siswa();
+        siswa destina = new siswa();
+        siswa latisa = new siswa();
+
+        zea.nama = "Azahra";
+        zea.absen = 6;
+        zea.ipk = 100;
+
+        System.out.println("Nama : " + zea.nama);
+        System.out.println("absen : " + zea.absen);
+        System.out.println("nilai : " + zea.ipk);
+
+        nimas.nama = "nimas putu";
+        nimas.absen = 30;
+        nimas.ipk = 100;
+
+        System.out.println("Nama : " + nimas.nama);
+        System.out.println("absen : " + nimas.absen);
+        System.out.println("nilai : " + nimas.ipk);
+
+
+        talitha.nama = "talitha Aurely";
+        talitha.absen = 34;
+        talitha.ipk = 100;
+
+        System.out.println("Nama : " + talitha.nama);
+        System.out.println("absen : " + talitha.absen);
+        System.out.println("nilai : " + talitha.ipk);
+
+        destina.nama = "destina";
+        destina.absen = 7;
+        destina.ipk = 100;
+
+        System.out.println("Nama : " + destina.nama);
+        System.out.println("absen : " + destina.absen);
+        System.out.println("nilai : " + destina.ipk);
+
+        latisa.nama = "Latisa Marta";
+        latisa.absen = 17;
+        latisa.ipk = 100;
+
+        System.out.println("Nama : " + latisa.nama);
+        System.out.println("absen : " + latisa.absen);
+        System.out.println("nilai : " + latisa.ipk);
+
+
+  }
+    
+}
