6a30329fad9e713266c76091105dacc4a42bea85
latisa-29 <latisa_sabela_33rpl@student.smktelkom-mlg.sch.id>
2024-08-23 13:51:44 +0700

    Add files via upload

--- karyawanTester.java
+++ karyawanTester.java
@@ -1,18 +1,32 @@
 //Driver Class
+
+import java.util.Scanner;
+
 public class karyawanTester {
     public static void main(String[] args) {
-    //object
-    //Class object = new constructor
-    karyawan tia = new karyawan(123, "tia", "tu", "malang");
-    karyawan alex = new karyawan(87, "alex", "bk", "surabaya");
-    karyawan rudi = new karyawan(9, "alex", "kesiswaan", "kediri");
-    karyawan surya = new karyawan(7, "surya", "kurikulum", "jember");
-    karyawan bunga = new karyawan(2, "bunga", "sapras", "banyuwangi");
+        Scanner input = new Scanner(System.in);
+        System.out.println("inputkan nama karyawan");
+        String nama = input.nextLine();
+        System.out.println("inputkan unit karyawan ");
+        String unit = input.nextLine();
+        System.out.println("masukkan alamat karyawan");
+        String alamat = input.nextLine();
+        System.out.println("inputkan id karyawan");
+        int id = input.nextInt();
 
-    tia.print();
-    alex.print();
-    rudi.print();
-    surya.print();
-    bunga.print();
+        // object
+        // Class object = new constructor
+        karyawan tia = new karyawan(123, "tia", "tu", "malang");
+        karyawan alex = new karyawan(87, "alex", "bk", "surabaya");
+        karyawan rudi = new karyawan(9, "alex", "kesiswaan", "kediri");
+        karyawan surya = new karyawan(7, "surya", "kurikulum", "jember");
+        karyawan bunga = new karyawan(2, "bunga", "sapras", "banyuwangi");
+
+        tia.print();
+        alex.print();
+        rudi.print();
+        surya.print();
+        bunga.print();
+        input.close();
     }
 }
