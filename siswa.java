551e7425ab2f0d7eb6bb2927c725c394f689eabb
latisa-29 <latisa_sabela_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 14:49:40 +0700

    Add files via upload

--- siswa.java
+++ siswa.java
@@ -1,16 +1,56 @@
 //Object class
 public class siswa {
- int absen;
- String nama;
- double ipk;
+  int absen;
+  String nama;
+  double ipk;
+
+  public int getAbsen() {
+    return this.absen;
+  }
+
+  public void setAbsen(int absen) {
+    this.absen = absen;
+  }
+
+  public String getNama() {
+    return this.nama;
+  }
+
+  public void setNama(String nama) {
+    this.nama = nama;
+  }
+
+  public double getIpk() {
+    return this.ipk;
+  }
+
+  public void setIpk(double ipk) {
+    this.ipk = ipk;
+  }
 
  //Constructor
  public siswa (){
-    absen = 0;
+   absen = 0;
     nama = "kosng";
     ipk = 0.0;
 
+ } 
+
+ //Constructor Parameter
+ public siswa (int id, String nama, double ipk) {
+   //variabel = parameter 
+   this.absen=absen; 
+   this.nama=nama;
+   this.ipk=ipk;
  }
+
+ //method print 
+ public void print() {
+   System.out.println("absen : " + absen);
+   System.out.println("nama : " + nama);
+   System.out.println("ipk : " + ipk);
+ }
+
 }
 
     
